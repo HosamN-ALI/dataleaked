babd87957726693e1341f5b01a8aef66f20afd2c
HosamN-ALI <93358299+HosamN-ALI@users.noreply.github.com>
2025-04-02 03:58:29 +0200

    Update extractor.bat

--- extractor.bat
+++ extractor.bat
@@ -5,7 +5,7 @@ REM Set the number of threads to the maximum available
 set "max_threads=80"
 
 REM Set the command template (Replace with your actual command, escaping special characters as needed)
-set "command_template=C:\Program Files\PeaZip\res\bin\7z\7z.exe x -aos -oC:\Users\Administrator\Downloads\New folder -bb0 -bse0 -bsp2 -p@fatecloud -sccUTF-8 -snz %%filename%% -r -- *password*.txt"
+set "command_template=C:\Program Files\PeaZip\res\bin\7z\7z.exe x -aos -oC:\Users\Administrator\Downloads\New folder -bb0 -bse0 -bsp2 -ppassword -sccUTF-8 -snz %%filename%% -r -- *password*.txt"
 
 REM Read file names from filenames.txt and run the command for each file
 for /f "tokens=*" %%i in (filenames.txt) do (
